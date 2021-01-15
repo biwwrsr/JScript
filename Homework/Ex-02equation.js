e0820db1f7ec0b02614c17801eb0c6f14f67957e
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-15 17:07:45 +0700

    Homework 1

---
+++ Homework/Ex-02equation.js
@@ -0,0 +1,26 @@
+function equation(text){
+    let n = text.lenght;
+    sum = 0;
+    for(i = 0 ; i < text.length ; i++){
+        if(i == 0){
+            sum = parseInt(text[i]);
+        }
+        if(text[i] == "+"){
+            i++;
+            num = parseInt(text[i]);
+            sum += num;
+        }
+        if(text[i] == "-"){
+            i++;
+            num = parseInt(text[i]);
+            sum -= num;
+        }
+        if(text[i] == "*"){
+            i++;
+            num = parseInt(text[i]);
+            sum *= num;
+        }
+    }
+    return sum;
+}
+console.log(equation("1+1+1+1+1"));
