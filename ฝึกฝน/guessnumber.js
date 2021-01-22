7f63a1b9661a7a28e2e398ae94718f27c16ff669
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-22 17:39:30 +0700

    guessnumber

---
+++ ฝึกฝน/guessnumber.js
@@ -0,0 +1,26 @@
+console.log("\n*********** Program Game Guess Number *************\n");
+num = Math.floor(Math.random() * 20) + 1;
+let input = "";
+
+function guessnumber(input){
+        console.log("You guess number is : "+ input + "\n");
+    if (input == num) {
+			done = false;
+			console.log("Lucky!!!!! \n");
+			console.log("You got 1,000,000 bath \n");
+			}
+		else {
+            console.log("This is wrong number");
+            if (input > num){
+               console.log(" ! more than");
+            }
+            else if (input < num){
+                 console.log(" ! less than");
+                }
+        }
+        return("\nThe correct number is : " + num + "\n");
+}
+
+console.log(guessnumber(13)); //input number
+
+
