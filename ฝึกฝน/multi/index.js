e0a78d7b39e08f2e14d1b7404cda9d6065e0925a
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-02-01 19:57:41 +0700

    update

---
+++ ฝึกฝน/multi/index.js
@@ -0,0 +1,17 @@
+let numberInput = document.getElementById('inputnumber');
+let Submit = document.getElementById('submit');
+let output = document.getElementById('output');
+
+function printMultiply(){
+    let number = Number(numberInput.value);
+    let outputhtml = '';
+
+    for (let i = 1; i<= 12; i++){
+        outputhtml+='<p>';
+        outputhtml+= number + ' x ' + i + ' = ' + (number*i);
+        outputhtml+='</p>';
+    }
+    output.innerHTML = outputhtml;
+}
+
+Submit.addEventListener('click', printMultiply);
