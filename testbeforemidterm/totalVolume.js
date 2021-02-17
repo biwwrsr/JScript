60fdce178bf125fbad71f5cdb2d4de94fa1bba0c
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-02-17 20:56:13 +0700

    update

--- testbeforemidterm/totalVolume.js
+++ testbeforemidterm/totalVolume.js
@@ -1,5 +1,5 @@
-function totalVolume(...boxes) {
-    return boxes.reduce((a,b)=> a + b.reduce((c,d)=> c * d,1),0);
+function totalVolume(...num) {
+    return num.reduce((a,b)=> a + b.reduce((c,d)=> c * d,1),0);
     }
  console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]))
  console.log(totalVolume([2, 2, 2], [2, 1, 1]))
